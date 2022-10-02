3a9ee5515441d241e9c8ff9bf770ff0d2fdbb767
Pedro Henrique <pedrohs21@outlook.com>
2022-10-02 20:02:48 -0300

    Remoção de logs.

--- aefa-monitor/sendToCloud.js
+++ aefa-monitor/sendToCloud.js
@@ -11,23 +11,19 @@ const cloud = axios.create({
 
 async function sendToCloud() {
     if(serverType == "local") {
-        console.log(("#".repeat(40) + "\n").repeat(3));
         try {
             const notCloudSaved = await Leitura.find({cloudSaved: false});
             for(var leitura of notCloudSaved) {
-                console.log(leitura);
                 try {
                     const response = await cloud.post("cloud-insert", leitura);
-                    console.log(response.data.statusLeitura);
                     if(response.data.statusLeitura == 'saved' || response.data.statusLeitura == 'updated') {
                         try {
-                                console.log("Entrou");
-                                leitura.cloudSaved = true;
-                                await leitura.save();
-                            } catch(error) {
-                                console.log("Erro ao atualizar localmente leitura salva em cloud");
-                                console.log(error.message);
-                            }
+                            leitura.cloudSaved = true;
+                            await leitura.save();
+                        } catch(error) {
+                            console.log("Erro ao atualizar localmente leitura salva em cloud");
+                            console.log(error.message);
+                        }
                     }
                 } catch(error) {
                     console.log("Erro no envio da leitura para cloud.");
